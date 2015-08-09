5ef99567740e27ffd9f7eb087be13865f7f99286
makfire <mukeshpandey15@hotmail.com>
2015-08-09 19:24:36 +0530

    changes settings file

--- knowyourmovie/knowyourmovie/settings.py
+++ knowyourmovie/knowyourmovie/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8#$e9u7nm9crj07uu#@-$y!qzmsd8y*ua%6an7=flecwy53vx_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
